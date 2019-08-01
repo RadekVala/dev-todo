5669882de59338a4f0b95ee8e09884765203d6f9
Radek Vala <radek.www@gmail.com>
2019-08-01 12:17:29 +0200

    Users seeder added

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+        ]);
+        factory(App\User::class, 5)->create();
+    }
+}
